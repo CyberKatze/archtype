0f24699767da7f0033e87450501de7d21731f23a
Mehrdad Shahidi <m.mehrdadshahidi@gmail.com>
2023-02-14 17:15:41 +0330

    ✨ Add simple css

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,6 @@
+export const load = () => {
+  return {
+    title: "Title 1",
+    summary: "This is some stupid summary about the title one."
+  }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts" >
+export let data: any;
+</script>
+
+
+<h1>Cool Website</h1>
+<main>
+  <content>
+    <h2>{data.title}</h2>
+    <p>
+      {data.summary}
+    </p>
+
+  </content>
+</main>
+<style>
+main {
+  max-width: 768px;
+  background-color: red;
+  margin: 0 auto;
+}
+</style>
